54bba5827854667406c4ba459c4b6a1c142adb27
菅原沙恵 <sugawarasae@sugawaraisagomegumi-no-MacBook-Pro.local>
2025-09-29 16:34:14 +0900

    Section14 ToDo新規作成機能の実装 ~一括代入(MassAssignment)~

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -23,12 +23,16 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
+        // フォームから送信された入力欄を連想配列で取得できる。
 
         $todo = new Todo(); 
         // todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo->content = $content;
-        // Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->user_id = Auth::id();
+        // user_idをフォームからしか受け取らない。
+        //  = 悪意のあるユーザーが、フォームを改ざんして他人のuser_idを指定しても無視される。
+        $todo->fill($inputs);
+        // ->fill()を使用することで、引数に指定した連想配列を一括代入できる。
         $todo->save();
         // Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
 
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $table = 'todos';
+
+    protected $fillable =
+    // $fillableの定義で、代入できる項目に制限をかける。
+    [
+        'content',
+    ];
 }
 
 ?>
