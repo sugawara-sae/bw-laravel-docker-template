e9c6b6dce4764e3a75749d21e07bc3968e2b2a4b
菅原沙恵 <sugawarasae@sugawaraisagomegumi-no-MacBook-Pro.local>
2025-09-29 15:04:24 +0900

    Section13 ToDo新規作成機能の実装 ~DBにデータを保存する~

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Todo;
+use Illuminate\Http\Request;
+
 
 class TodoController extends Controller
 {
@@ -18,4 +20,20 @@ class TodoController extends Controller
     {
         return view('todo.create');
     }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+
+        $todo = new Todo(); 
+        // todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $todo->content = $content;
+        // Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->save();
+        // Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+
+        return redirect()->route('todo.index');
+    }
 }
+
+?>
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+Route::get('/',function(){
+  return view('welcome');
+});
+
 Route::get('/todo', 'TodoController@index');
 Route::get('/todo/create', 'TodoController@create');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+Route::post('/todo', 'TodoController@store')->name('todo.store');
+Route::get('/todo', 'TodoController@index')->name('todo.index');
