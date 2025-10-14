d1cacf4ea3515f2a670e84bd4d0637f3c3459b17
菅原沙恵 <sugawarasae@sugawaraisagomegumi-no-MacBook-Pro.local>
2025-10-14 11:39:47 +0900

    Section20 バリデーションの実装

--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -24,6 +24,14 @@
                 @if($errors->has('content'))
                 <span class="text-danger">{{ $errors->first('content') }}</span>
                 @endif
+                <!-- Section20
+                バリデーション実装。class内の設定追加。
+                バリデーションエラーとなった項目が存在した場合、対応するエラーメッセージが自動的にセッションに保存される。
+                セッションに保存されたメッセージは、グローバル変数 $errors を通してどのBladeからでも取得できる。
+                $errors は MessageBagクラスのインスタンスが代入されており、発生したバリデーションエラーの情報を持っている。
+                $errors->has('入力欄のname属性') = その入力欄でバリデーションエラーが発生しているか判定。
+                $errors->first('入力欄のname属性') = その入力欄で最初に発生したエラーメッセージを出力。 
+                このセッションは1リクエスト間のみ有効で、その後は自動的に削除される。-->
               </div>
             </div>
             <div class="form-group row mb-0">
